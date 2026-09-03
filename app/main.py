563102286e650a04a0ae00f0d7c6ca46dbb3b7ed
Mithran J <40892267+Iam-mithran@users.noreply.github.com>
2026-09-03 15:09:36 +0530

    Add import for os module in main.py

--- app/main.py
+++ app/main.py
@@ -13,6 +13,7 @@ Run it locally:   uvicorn app.main:app --reload
 Then open:        http://127.0.0.1:8000/docs
 """
 
+import os
 import logging
 
 from fastapi import FastAPI, HTTPException, Response, status
